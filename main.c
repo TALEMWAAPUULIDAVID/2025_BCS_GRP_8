cbc17b8a0e7646b83c31f67fca2a21bceaf79b16
TALEMWADAVID <talemwadapuuli@gmail.com>
2025-09-08 16:18:59 +0300

    STAGE 1:  commit message

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "calculator.h.H"
+// Declaring of Global variable
+int result = 0;
+
+
+// Function Methods
+int sum (int x, int y){
+    result = x + y;
+    return result;
+}
+int sub (int x , int y){
+    result = x - y ;
+    return result;
+}
+int mul (int x , int y){
+    result = x * y ;
+    return result;
+}
+int divide (int x , int y){
+    result = x / y ;
+    return result;
+}
+int mod (int x , int y){
+    result = x % y ;
+    return result;
+}
+
 int main()
 {
+
     // Simple Calculator menu display
+
     printf("\n==== Simple Calculator ====\n");
     printf("1. Addition (+)\n");
     printf("2. Subtraction (-)\n");
@@ -12,60 +41,76 @@ int main()
     printf("5. Modulus (%%)\n");
 
     // Declaring of variables to be used in the simple calculator
-    char operator = '\0';
-    int first_number ;
-    float second_number = 0.0f;
-    float result = 0.0f;
+
+    int operator = 0;
+    int first_number = 0;
+    int second_number = 0;
+    int result = 0;
+
     // Prompting the user to enter his/her data
+
     printf("Enter first_number: ");
     scanf("%d", &first_number);
 
-    printf("Enter the operator(+ - * / %% ): ");
-    scanf(" %c", &operator);
+    
 
     printf("Enter the second_number: ");
-    scanf("%f",&second_number);
+    scanf("%d",&second_number);
+
+    printf("Enter the operation(1-Addition(+),2-Subtraction(-),3-Multiplication(*),4-Division(/),5-Modulus(%%)): ");
+    scanf(" %d", &operator);
+    
     //Displaying of the user input for confirmation
     printf("\nDear user, you have entered: \n");
-    printf("first_number = %d\n", first_number);
-    printf("operator = %c\n", operator);
-    printf("second_number = %.4f\n", second_number);
-    // Performing of calculations according to opertaor chosen by user
+
+    printf("Enter first_number : %d\n", first_number);
+    
+    printf("Enter second_number : %d\n", second_number);
+
+    printf("Enter operation(1-5) : %d\n", operator);
+
+    // Performing of calculations according to opertaor chosen by user by switch case
+
     switch(operator)
     {
-        case '+':
-            result = first_number + second_number;
+        case 1:
+            result = sum(first_number, second_number);
+            printf("Result = %d + %d= %d\n", first_number, second_number, result);
             break;
-        case '-':
-            result = first_number - second_number;
+        case 2:
+            result = sub(first_number, second_number);
+            printf("Result = %d - %d= %d\n", first_number, second_number, result);
             break;
-        case '*':
-            result = (float)first_number * second_number;
+        case 3:
+            result = mul(first_number, second_number);
+            printf("Result = %d * %d= %d\n", first_number, second_number, result);
             break;
-        case '/':
+        case 4:
             if (second_number == 0){
                 printf("Math Error\n");
             }
             else{
-                result = (float)first_number / second_number;
+                result = divide(first_number, second_number);
+
+                printf("Result = %d / %d= %d\n", first_number, second_number, result);
             }
             break;
-        case '%':
+        case 5:
             if (second_number == 0)
-            {printf("Math Error\n");
+            {printf("Error:Division by zero is not allowed\n");
         }
         else{
-            result = (int)first_number % (int)second_number;
+            result = mod(first_number, second_number);
+            printf("Result = %d %% %d= %d\n", first_number, second_number, result);
         }
         break;
         default:
             printf("kindly try other operators\n");
 
     }
-    //Disply of result to user
-    printf("Result = %.5f",result);
 
-    
+
+
 
     return 0;
 }
